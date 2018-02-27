ad4adb910bf8b7d8f13c6b7b4c082b43ec728238
Alfredo <atakori@outlook.com>
2018-02-26 20:54:25 -0500

    added scroll animation for projects section

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Scroll to section
 $('.about_me_button').click(function() {
 		$('html, body').animate({
 	        scrollTop: $(".about_me_info").offset().top
@@ -15,3 +16,22 @@ $('.contact_me_button').click(function() {
 	        scrollTop: $(".contact_me_section").offset().top
 	    }, 800);
 	});
+
+//Scroll to projects
+$('.now_playing_button').click(function() {
+		$('html, body').animate({
+	        scrollTop: $(".now_playing_info").offset().top
+	    }, 1000);
+});
+
+$('.mark_it_button').click(function() {
+		$('html, body').animate({
+	        scrollTop: $(".mark_it_info").offset().top
+	    }, 1000);
+});
+
+$('.checkpoint_button').click(function() {
+		$('html, body').animate({
+	        scrollTop: $(".checkpoint_info").offset().top
+	    }, 1000);
+});
