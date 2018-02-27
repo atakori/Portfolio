5de6a1ab4299c827d592e281cb3d8430092b724a
Alfredo <atakori@outlook.com>
2018-02-26 20:29:16 -0500

    added button scrolling interactivity for welcome section

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 $('.about_me_button').click(function() {
-	alert('working');
-})
+		$('html, body').animate({
+	        scrollTop: $(".about_me_info").offset().top
+	    }, 1000);
+});
+
+$('.my_projects_button').click(function() {
+		$('html, body').animate({
+	        scrollTop: $(".projects_section").offset().top
+	    }, 1000);
+});
+
+$('.contact_me_button').click(function() {
+		$('html, body').animate({
+	        scrollTop: $(".contact_me_section").offset().top
+	    }, 800);
+	});
